78e3ee332bc193f02dd2de0e500cdf6761a345a9
Tatsuya Ishikawa <34103899+tatsuyai713@users.noreply.github.com>
2026-09-01 09:20:09 +0000

    Update startwm.sh to use kwriteconfig6 and add checks

--- files/kde-root/defaults/startwm.sh
+++ files/kde-root/defaults/startwm.sh
@@ -37,11 +37,14 @@ if [ -n "${KWRITECONFIG}" ]; then
 fi
 
 # Disable compositing and screen lock
-if [ ! -f $HOME/.config/kwinrc ]; then
-  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
-fi
-if [ ! -f $HOME/.config/kscreenlockerrc ]; then
-  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+KWRITECONFIG="$(command -v kwriteconfig6 || command -v kwriteconfig5 || true)"
+if [ -n "$KWRITECONFIG" ]; then
+  if [ ! -f $HOME/.config/kwinrc ]; then
+    "$KWRITECONFIG" --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+  fi
+  if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+    "$KWRITECONFIG" --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+  fi
 fi
 
 # Power related
